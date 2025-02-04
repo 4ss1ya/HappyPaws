1d50b08200fb4459ff81ca406fc9da2240ac5f82
4ss1ya <bb84960@gmail.com>
2025-02-04 19:05:08 +0500

    db

--- app/src/main/java/com/example/happypaws/MainActivity.java
+++ app/src/main/java/com/example/happypaws/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.happypaws;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -20,5 +21,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        SQLiteDatabase db = getBaseContext()
+                .openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS People ( id_People INTEGER PRIMARY KEY AUTOINCREMENT, id_pet INTEGER, Name TEXT NOT NULL, Number TEXT, Adres TEXT, FOREIGN KEY (id_pet) REFERENCES Pet(id_Pet) ON DELETE SET NULL)");
+
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Pet ( id_Pet INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Birthday DATE, Gender TEXT, id_type INTEGER, PHOTO BLOB, FOREIGN KEY (id_type) REFERENCES Type(id_type) ON DELETE SET NULL)");
+
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Type ( id_type INTEGER PRIMARY KEY AUTOINCREMENT, NameType TEXT NOT NULL, who TEXT)");
+
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Books ( id_Books INTEGER PRIMARY KEY AUTOINCREMENT, pet_id INTEGER, text TEXT, FOREIGN KEY (pet_id) REFERENCES Pet(id_Pet) ON DELETE CASCADE)");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS vaccine ( id_vaccine INTEGER PRIMARY KEY AUTOINCREMENT, pet_id INTEGER, text TEXT, FOREIGN KEY (pet_id) REFERENCES Pet(id_Pet) ON DELETE CASCADE)");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Events ( id_Events INTEGER PRIMARY KEY AUTOINCREMENT, Date DATE NOT NULL, text TEXT, is_done BOOLEAN DEFAULT 0)");
+
+
     }
 }
